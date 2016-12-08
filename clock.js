3134f4f6eca9d92a99739a472c1b081cc7aaa02b
Steve Fiedler <sfiedler@gmail.com>
2016-12-08 18:32:43 +1000

    time with start pause and stop

--- clock.js
+++ clock.js
@@ -1,10 +1,9 @@
 // Global variables
+var timerId;
+var countdownTime = 0;
+var timerHasLooped = 0;
+var timerHasStarted = 0;
 
-var clock;
-var currentTime = 0;
-var sessionTime = 1;
-var breakTime;
-var countdownTime;
 
 // Functions
 function millisecondsToSeconds(time) {
@@ -26,26 +25,81 @@ function secondsToMMSS(time) {
 	return [minutesRemaining, secondsRemaining];
 }
 
-function countdown(){
 
+
+function countdown(){
 	
+	timerHasStarted = 1;
+
 	document.getElementById("clock").innerHTML = countdownTime;
 
 	countdownTime -= 1;
 
-	if(countdownTime === 0){
-		clearInterval(timerId)
+	if(countdownTime < 0){
+		clearInterval(timerId);
+		if (timerHasLooped === 0){
+			startBreak();
+			timerHasLooped = 1;
+		}
+		else{
+			startCountDownClock();
+			timerHasLooped = 0;
+		}
+		
 	}
+
+}
+
+function startCountDown(){
+	if (timerHasStarted === 0){
+		startCountDownClock();
+		}
+	else{
+		timerId = setInterval(countdown, 1000);
+	}
+}
+
+function startCountDownClock(){
+		var sessionTime = getSessionLength();
+		countdownTime = sessionTime;
+		timerId = setInterval(countdown, 1000);
+
+}
+
+function getSessionLength() {
+	var sessionTime = 0;
+	var sessionLength = document.getElementById("sessionLength").value;
+	return sessionTime = parseInt(sessionLength);
+}
+
+function getbreakLength() {
+	var breakTime = 0;
+	var breakLength = document.getElementById("breakLength").value;
+	return breakTime = parseInt(breakLength);
+}
+function stopCountDown(){
+	clearInterval(timerId);
+	document.getElementById("clock").innerHTML = "0";
+	timerHasStarted = 0;
+}
+
+function pauseCountDown() {
+	clearInterval(timerId);
+}
+function startBreak() {
+	var breakTime = 0;
+
+	var breakTime = getbreakLength();
+
+	countdownTime = breakTime;
+	timerId = setInterval(countdown, 1000);
 
 
 }
-// Programme logic
 
-// countdownTime = minutesToSeconds(sessionTime)
 
-countdownTime = 5;
 
-var timerId = setInterval(countdown, 1000);
+
 
 
 
